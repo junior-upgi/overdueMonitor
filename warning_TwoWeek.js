11360800181a0c7ada7635dbc20a4963cc8135c3
Chia-Yu Tsai <Chia-Yu Tsai>
2016-10-04 17:34:18 +0800

    code clean up

--- warning_TwoWeek.js
+++ warning_TwoWeek.js
@@ -9,7 +9,16 @@ var mssqlConfig = {
     user: 'sunlikeReader', // same account (production/development)
     password: 'sunlikeReader', // same password (production/development)
     server: '192.168.99.1' // 192.168.168.2 (current production ERP server - sunv9)
-}
+};
+
+// host for the mobile messaging system
+var mysqlConfig = {
+    host: 'upgi.ddns.net', // when the APP is moved to the production server, should change to 'localhost'
+    port: '3306',
+    user: 'overdueMonitor', // this does not change, APP has to remote access production server
+    password: 'overdueMonitor', // this does not change, APP has to remote access production server
+    charset: 'utf8_bin'
+};
 
 // connect to ERP server
 mssql.connect(mssqlConfig, function (err) {
@@ -22,28 +31,22 @@ mssql.connect(mssqlConfig, function (err) {
             var recipientID = "";
             var messageID = utility.uuidGenerator();
             var broadcastStatusID = utility.uuidGenerator();
-            // host for the mobile messaging system 
-            var mysqlConn = mysql.createConnection({
-                host: 'upgi.ddns.net', // when the APP is moved to the production server, should change to 'localhost'
-                port: '3306',
-                user: 'overdueMonitor', // this does not change, APP has to remote access production server
-                password: 'overdueMonitor', // this does not change, APP has to remote access production server
-                charset: 'utf8_bin'
-            });
+            // establish connection to MySQL
+            var mysqlConn = mysql.createConnection(mysqlConfig);
             mysqlConn.connect();
             // check the user ID used by the mobile messaging system (compare against the particular sales' ERP ID or 員工編號)
             mysqlConn.query("SELECT a.userID,c.erpID FROM upgiSystem.userGroupMembership a INNER JOIN (SELECT ID FROM upgiSystem.userGroup WHERE reference='Sales') b ON a.userGroupID=b.ID LEFT JOIN upgiSystem.user c ON a.userID=c.ID WHERE a.deprecated IS NULL AND c.erpID='" + item.recipientID + "';", function (err, data, fieldList) {
                 if (err) throw err;
                 recipientID = data[0].userID;
                 // write to the mobileMessagingSystem.message table
-                mysqlConn.query("INSERT INTO mobileMessagingSystem.message (`ID`,`messageCategoryID`,`systemCategoryID`,`manualTopic`,`content`,`created_at`) VALUES ('" + messageID + "'," + item.messageCategoryID + "," + item.systemCategoryID + ",'" + item.manualTopic + "','" + item.content + "',NOW());", function (err, data, fieldList) {
+                mysqlConn.query("INSERT INTO mobileMessagingSystem.message (`ID`,`messageCategoryID`,`systemCategoryID`,`manualTopic`,`content`,`created_at`) VALUES ('" + messageID + "'," + item.messageCategoryID + "," + item.systemCategoryID + ",'" + item.manualTopic + "','" + item.content + "',NOW());", function (err) {
                     if (err) throw err;
                     // write the mobileMessagingSystem.broadcastStatus table
-                    mysqlConn.query("INSERT INTO mobileMessagingSystem.broadcastStatus (`ID`,`messageID`,`recipientID`,`primaryRecipient`,`url`,`audioFile`,`permanent`,`created_at`) VALUES ('" + broadcastStatusID + "','" + messageID + "','" + recipientID + "','1',NULL,'siren.mp3',0,NOW());", function (err, data, fieldList) {
+                    mysqlConn.query("INSERT INTO mobileMessagingSystem.broadcastStatus (`ID`,`messageID`,`recipientID`,`primaryRecipient`,`url`,`audioFile`,`permanent`,`created_at`) VALUES ('" + broadcastStatusID + "','" + messageID + "','" + recipientID + "','1',NULL,'siren.mp3',0,NOW());", function (err) {
                         if (err) throw err;
-                        mysqlConn.end();
-                        mssql.close();
                     });
+                    mysqlConn.end();
+                    mssql.close();
                 });
             });
         });
