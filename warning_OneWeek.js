3ac3f8f83e37474bb8de7d7269b38c7b8a02b1fe
Chia-Yu Tsai <Chia-Yu Tsai>
2016-10-04 17:51:29 +0800

    code clean up

--- warning_OneWeek.js
+++ warning_OneWeek.js
@@ -34,21 +34,25 @@ mssql.connect(mssqlConfig, function (err) {
             // establish connection to MySQL
             var mysqlConn = mysql.createConnection(mysqlConfig);
             mysqlConn.connect();
+            // write to the mobileMessagingSystem.message table
+            mysqlConn.query("INSERT INTO mobileMessagingSystem.message (`ID`,`messageCategoryID`,`systemCategoryID`,`manualTopic`,`content`,`created_at`) VALUES ('" + messageID + "'," + item.messageCategoryID + "," + item.systemCategoryID + ",'" + item.manualTopic + "','" + item.content + "',NOW());", function (err) {
+                if (err) throw err;
+            });
             // check the user ID used by the mobile messaging system (compare against the particular sales' ERP ID or 員工編號)
             mysqlConn.query("SELECT a.userID,c.erpID FROM upgiSystem.userGroupMembership a INNER JOIN (SELECT ID FROM upgiSystem.userGroup WHERE reference='Sales') b ON a.userGroupID=b.ID LEFT JOIN upgiSystem.user c ON a.userID=c.ID WHERE a.deprecated IS NULL AND c.erpID='" + item.recipientID + "';", function (err, data, fieldList) {
                 if (err) throw err;
                 recipientID = data[0].userID;
-                // write to the mobileMessagingSystem.message table
-                mysqlConn.query("INSERT INTO mobileMessagingSystem.message (`ID`,`messageCategoryID`,`systemCategoryID`,`manualTopic`,`content`,`created_at`) VALUES ('" + messageID + "'," + item.messageCategoryID + "," + item.systemCategoryID + ",'" + item.manualTopic + "','" + item.content + "',NOW());", function (err) {
+                // write the mobileMessagingSystem.broadcastStatus table
+                mysqlConn.query("INSERT INTO mobileMessagingSystem.broadcastStatus (`ID`,`messageID`,`recipientID`,`primaryRecipient`,`url`,`audioFile`,`permanent`,`created_at`) VALUES ('" + broadcastStatusID + "','" + messageID + "','" + recipientID + "','1',NULL,'siren.mp3',0,NOW());", function (err) {
                     if (err) throw err;
-                    // write the mobileMessagingSystem.broadcastStatus table
-                    mysqlConn.query("INSERT INTO mobileMessagingSystem.broadcastStatus (`ID`,`messageID`,`recipientID`,`primaryRecipient`,`url`,`audioFile`,`permanent`,`created_at`) VALUES ('" + broadcastStatusID + "','" + messageID + "','" + recipientID + "','1',NULL,'siren.mp3',0,NOW());", function (err) {
-                        if (err) throw err;
-                    });
-                    mysqlConn.end();
-                    mssql.close();
                 });
+                closeDataConnection(mysqlConn, mssql);
             });
         });
     });
 });
+
+function closeDataConnection(mysqlConn, mssqlConn) {
+    mysqlConn.end();
+    mssqlConn.close();
+}
