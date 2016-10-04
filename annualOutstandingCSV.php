ab8a295f3a709dbde8ac007cb0d93d09862c9d8e
Chia-Yu Tsai <Chia-Yu Tsai>
2016-10-04 09:13:52 +0800

    fixed php script to CSV    - corrected table names start first draft of a node js backend server

--- annualOutstandingCSV.php
+++ annualOutstandingCSV.php
@@ -11,7 +11,7 @@ echo iconv("UTF-8","big5","應收款項資料年度:");
 echo $_GET["YEAR"]."\n";
 echo iconv("UTF-8","big5","客戶名稱,付款條件,一月,二月,三月,四月,五月,六月,七月,八月,九月,十月,十一月,十二月,逾期總額,預收總額,催繳次數,責任業務\n");
 
-$res = $connection->query("SELECT * FROM UPGI_OverdueMonitor.dbo.annualReport where YEAR='".$_GET["YEAR"]."' order by CUS_SNM");
+$res = $connection->query("SELECT * FROM UPGI_OverdueMonitor.dbo.annualReportDetail where YEAR='".$_GET["YEAR"]."' order by CUS_SNM");
 	foreach ($res->fetchAll(PDO::FETCH_ASSOC) as $row) {
 	
 		echo iconv("UTF-8","BIG5",$row["CUS_SNM"].",");
