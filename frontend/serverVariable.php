c6a839e65b8bb65a2540358618a05d90ae9cbcde
Chia-Yu Tsai <Chia-Yu Tsai>
2016-10-05 13:54:51 +0800

    fixed comments

--- frontend/serverVariable.php
+++ frontend/serverVariable.php
@@ -1,7 +1,8 @@
 <?php
     $user = 'sunlikeReader'; 
     $password = 'sunlikeReader';
-    $host = 'svd13216pwb'; //production server is to be connected to 'sunv9'
+    // 'sunv9' for production server, 'svd13216pwb' for local test env.
+    $host = 'svd13216pwb';
     $dsn = 'odbc:DRIVER=FreeTDS;SERVERNAME='.$host.';dbname=UPGI_OverdueMonitor;charset=UTF-8';
     $option = array('PDO::ATTR_ERRMODE' => 'PDO::ERRMODE_EXCEPTION', 'PDO::ATTR_DEFAULT_FETCH_MODE' => 'PDO::FETCH_ASSOC', 'PDO::ATTR_EMULATE_PREPARES' => 'false');
 ?>
