c65399dc6b4f3c36050e28ad2d06188cd80d4db7
junior-upgi <junior@upgi.com.tw>
2016-11-12 17:27:06 +0800

    code maintenance

--- backend/broadcast.js
+++ backend/broadcast.js
@@ -3,7 +3,7 @@ var app = express();
 var bodyParser = require('body-parser');
 var mysql = require('mysql');
 var utility = require('./uuidGenerator.js');
-var moment = require('moment');
+var moment = require('moment-timezone');
 
 app.use(bodyParser.json());
 
@@ -26,27 +26,48 @@ app.post('/broadcast', function(req, res) {
     // establish connection to mobileMessagingSystem server
     var mysqlConn = mysql.createConnection(mysqlConfig);
     mysqlConn.connect();
-    mysqlConn.query("INSERT INTO mobileMessagingSystem.message (`ID`,`messageCategoryID`,`systemCategoryID`,`manualTopic`,`content`,`created_at`) VALUES ('" + messageID + "'," + message.messageCategoryID + "," + message.systemCategoryID + ",'" + message.manualTopic + "','" + message.content + "','" + currentDatetime + "');", function(error) {
-        if (error) throw error;
-    });
+    mysqlConn.query("INSERT INTO mobileMessagingSystem.message (`ID`,`messageCategoryID`,`systemCategoryID`,`manualTopic`,`content`,`created_at`) VALUES ('" +
+        messageID + "'," +
+        message.messageCategoryID + "," +
+        message.systemCategoryID + ",'" +
+        message.manualTopic + "','" +
+        message.content + "','" +
+        currentDatetime + "');",
+        function(error) {
+            if (error) {
+                console.log("mobileMessagingSystem.message 資料寫入錯誤：" + error);
+                res.status(500).send("mobileMessagingSystem.message 資料寫入錯誤：" + error).end();
+            }
+        });
     mysqlConn.query("SELECT a.userID,c.erpID FROM upgiSystem.userGroupMembership a INNER JOIN (SELECT ID FROM upgiSystem.userGroup WHERE reference='" + message.userGroup + "') b ON a.userGroupID=b.ID LEFT JOIN upgiSystem.user c ON a.userID=c.ID WHERE a.deprecated IS NULL AND c.erpID='" + message.recipientID + "';", function(error, data, fieldList) {
-        if (error) throw error;
+        if (error) {
+            console.log("員工帳號查詢發生錯誤：" + error);
+            res.status(500).send("員工帳號查詢發生錯誤：" + error).end();
+        }
+        console.log(data === []);
         recipientID = data[0].userID;
         // write the mobileMessagingSystem.broadcastStatus table
-        mysqlConn.query("INSERT INTO mobileMessagingSystem.broadcastStatus (`ID`,`messageID`,`recipientID`,`primaryRecipient`,`url`,`audioFile`,`permanent`,`created_at`) VALUES ('" + broadcastStatusID + "','" + messageID + "','" + recipientID + "','1','" + message.url + "','" + message.audioFile + "',0,'" + currentDatetime + "');", function(error) {
-            if (error) throw error;
-        });
+        mysqlConn.query("INSERT INTO mobileMessagingSystem.broadcastStatus (`ID`,`messageID`,`recipientID`,`primaryRecipient`,`url`,`audioFile`,`permanent`,`created_at`) VALUES ('" +
+            broadcastStatusID + "','" +
+            messageID + "','" +
+            recipientID +
+            "','1','" +
+            message.url + "','" +
+            message.audioFile +
+            "',0,'" +
+            currentDatetime + "');",
+            function(error) {
+                if (error) {
+                    console.log("mobileMessagingSystem.broadcastStatus 資料寫入錯誤：" + error);
+                    res.status(500).send("mobileMessagingSystem.broadcastStatus 資料寫入錯誤：" + error).end();
+                }
+            });
         //close connection
         mysqlConn.end();
     });
-    console.log("==========" + currentDatetime + " 開始推撥 ==========\n");
-    console.log(message + "\n");
-    console.log("==========" + currentDatetime + " 推撥完畢 ==========\n");
-    res.send(
-        "<div>==========" + currentDatetime + " 開始推撥 ==========</div>" +
-        "<p>" + message + "</p>" +
-        "<div>==========" + currentDatetime + " 推撥完畢 ==========</div>");
+    console.log(moment(moment(), "YYYY-MM-DD HH:mm:ss").format("YYYY-MM-DD HH:mm:ss") + " 訊息已推播");
+    res.status(200).send(moment(moment(), "YYYY-MM-DD HH:mm:ss").format("YYYY-MM-DD HH:mm:ss") + " 訊息已推播").end();
 });
 
 app.listen(3939);
-console.log('broadcast backend server running on port 3939...\n');
+console.log("推播服務程式運行中... (http://upgi.ddns.net:3939/broadcast)");
