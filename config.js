ec414ea2244232d20a3e9788c4a1760d5358c058
junior-upgi <junior@upgi.com.tw>
2016-12-01 18:38:54 +0800

    daily update fixed

--- config.js
+++ config.js
@@ -1,10 +1,14 @@
-var serverHost = "http://localhost"; // development
-//var serverHost = "http://192.168.168.25"; // production
-var serverPort = process.env.PORT || 9003;
+"use strict";
+
+var serverHost = "http://localhost"; //development
+var serverPort = process.env.PORT || 9002;
+var broadcastServerHost = "http://upgi.ddns.net";
+var broadcastServerPort = process.env.PORT || 9001;
+var broadcastAPIUrl = broadcastServerHost + ":" + broadcastServerPort + "/broadcast";
 var mssqlServerHost = "http://upgi.ddns.net"; // access database from the internet (development)
 //var mssqlServerHost = "http://192.168.168.5"; // access database from LAN (production)
-var upgiSystemAccount = "overdueMonitor";
-var upgiSystemPassword = "overdueMonitor";
+var upgiSystemAccount = "upgiSystem";
+var upgiSystemPassword = "upgiSystem";
 
 var mssqlConfig = {
     server: mssqlServerHost.slice(7),
@@ -12,13 +16,19 @@ var mssqlConfig = {
     password: upgiSystemPassword
 };
 
+const smtpTransportAccount = "smtps://junior.upgi@gmail.com:cHApPPZV@smtp.gmail.com";
+
 const workingTimezone = "Asia/Taipei";
 
 module.exports = {
     serverHost,
     serverPort,
+    broadcastServerHost,
+    broadcastServerPort,
+    broadcastAPIUrl,
     upgiSystemAccount,
     upgiSystemPassword,
     mssqlConfig,
+    smtpTransportAccount,
     workingTimezone
 };
--- overdueMonitor.js
+++ overdueMonitor.js
@@ -1,77 +1,99 @@
+"use strict";
+
 var moment = require("moment-timezone");
 var CronJob = require("cron").CronJob;
-var mssql = require("mssql");
-var httpRequest = require("request");
+var httpRequest = require("request-promise");
+var Q = require("q");
 
-var queryString = require("./model/queryString.js");
 var config = require("./config.js");
+var database = require("./module/database.js");
+var queryString = require("./model/queryString.js");
+var telegramBot = require("./model/telegramBot.js");
+var telegramChat = require("./model/telegramChat.js");
+var telegramUser = require("./model/telegramUser.js");
+var upgiSystem = require("./model/upgiSystem.js");
 
-var dailyMonitorTask = new CronJob("0 0 9 * * *", function() {
-    var mssqlConnection = new mssql.Connection(config.mssqlConfig);
-    var mssqlRequest;
-    mssqlConnection.connect()
-        .then(function() {
-            mssqlRequest = new mssql.Request(mssqlConnection);
-            mssqlRequest.query(queryString.newOverdueAlarmQuery)
-                .then(function(newOverdueList) {
-                    newOverdueList.forEach(function(newOverdue) {
-                        console.log(newOverdue.verboseMessage);
-                        httpRequest.post("http://upgi.ddns.net:9001/broadcast") // to me
-                            .form({
-                                chat_id: 241630569,
+//var newOverdueGroupMessage = ""; // string to hold group message of new overdue records
+var dailyMonitorTask = new CronJob(upgiSystem.list[1].jobList[0].schedule, function() {
+    var broadcastTargetIDList = []; // list to hold a list of broadcast recipients
+    var newOverdueGroupMessage = "【新增逾期款項】"; // reinitialize
+    if (upgiSystem.list[1].jobList[0].online === true) { // only execute the cron job if the 'online' property is true
+        console.log(
+            "\n" + moment(moment(), "YYYY-MM-DD HH:mm:ss").format("YYYY-MM-DD HH:mm:ss") + " " +
+            "proceeding with scheduled [" + upgiSystem.list[1].jobList[0].id + "]"
+        );
+        // query the database for new overdue records
+        database.executeQuery(queryString.newOverdueAlarmQuery, function(newOverdueList, error) {
+            if (error) {
+                return console.log(error);
+            }
+            broadcastTargetIDList = upgiSystem.list[1].jobList[0].targetUserIDList.slice();
+            newOverdueList.forEach(function(newOverdue) { // loop through each record in the list
+                newOverdueGroupMessage += "\n   " + newOverdue.content;
+                broadcastTargetIDList.push(telegramUser.getUserID(newOverdue.SAL_NAME)); // add staff that came up in the current record
+                if (upgiSystem.list[1].jobList[0].broadcast === true) { // only broadcast if the 'broadcast' property is true
+                    broadcastTargetIDList.forEach(function(broadcastTargetID) { // loop through broadcastTargetIDList and broadcast
+                        httpRequest({ // broadcast individual message
+                            method: "post",
+                            uri: config.broadcastAPIUrl,
+                            form: {
+                                chat_id: broadcastTargetID,
                                 text: newOverdue.verboseMessage,
-                                token: "267738010:AAGT17aLumIfVPNeFWht8eUEPuC2HfAouGk"
-                            });
-                        httpRequest.post("http://upgi.ddns.net:9001/broadcast") // to it chatroom
-                            .form({
-                                chat_id: -157638300,
-                                text: newOverdue.verboseMessage,
-                                token: "267738010:AAGT17aLumIfVPNeFWht8eUEPuC2HfAouGk"
-                            });
+                                token: telegramBot.getToken("overdueMonitorBot")
+                            }
+                        });
                     });
-                    mssqlConnection.close();
-                })
-                .catch(function(error) {
-                    return console.log("overdueMonitor.dbo.warning_NewOverdue 查詢發生錯誤： " + error);
-                });
-        })
-        .catch(function(error) { // connect failure
-            return console.log("資料庫連結發生錯誤： " + error);
+                }
+            });
+            httpRequest({ // broadcast group message
+                method: "post",
+                uri: config.broadcastAPIUrl,
+                form: {
+                    chat_id: telegramChat.getChatID("業務群組"),
+                    text: newOverdueGroupMessage,
+                    token: telegramBot.getToken("overdueMonitorBot")
+                }
+            });
         });
+    }
 }, null, true, config.workingTimezone);
 dailyMonitorTask.start();
 
-var weeklyMonitorTask = new CronJob("0 0 9 * * 1", function() {
-    var mssqlConnection = new mssql.Connection(config.mssqlConfig);
-    var mssqlRequest;
-    mssqlConnection.connect()
-        .then(function() {
-            mssqlRequest = new mssql.Request(mssqlConnection);
-            mssqlRequest.query(queryString.WeeklySummaryQuery)
-                .then(function(newOverdueList) {
-                    newOverdueList.forEach(function(newOverdue) {
-                        console.log(newOverdue.verboseMessage);
-                        httpRequest.post("http://upgi.ddns.net:9001/broadcast") // to me
-                            .form({
-                                chat_id: 241630569,
-                                text: newOverdue.verboseMessage,
-                                token: "267738010:AAGT17aLumIfVPNeFWht8eUEPuC2HfAouGk"
-                            });
-                        httpRequest.post("http://upgi.ddns.net:9001/broadcast") // to it chatroom
-                            .form({
-                                chat_id: -157638300,
-                                text: newOverdue.verboseMessage,
-                                token: "267738010:AAGT17aLumIfVPNeFWht8eUEPuC2HfAouGk"
-                            });
-                    });
-                    mssqlConnection.close();
-                })
-                .catch(function(error) {
-                    return console.log("overdueMonitor.dbo.warning_NewOverdue 查詢發生錯誤： " + error);
-                });
-        })
-        .catch(function(error) { // connect failure
-            return console.log("資料庫連結發生錯誤： " + error);
+/*
+var weeklyMonitorTask = new CronJob(upgiSystem.list[1].jobList[1].schedule, function() {
+    if (upgiSystem.list[1].jobList[1].online === true) {
+        console.log(
+            moment(moment(), "YYYY-MM-DD HH:mm:ss").format("YYYY-MM-DD HH:mm:ss") + " " +
+            "proceeding with scheduled [" + upgiSystem.list[1].jobList[1].id + "]"
+        );
+        database.executeQuery(queryString.pastWeekOverdueAlarmQuery, function(pastWeekOverdueList, error) {
+            if (error) {
+                return console.log(error);
+            }
+            pastWeekOverdueList.forEach(function(pastWeekOverdue) {
+                console.log(pastWeekOverdue.content);
+            });
         });
+        database.executeQuery(queryString.oneWeekWarningQuery, function(oneWeekWarningList, error) {
+            if (error) {
+                return console.log(error);
+            }
+            oneWeekWarningList.forEach(function(oneWeekWarning) {
+                console.log(oneWeekWarning.content);
+            });
+        });
+        database.executeQuery(queryString.twoWeekWarningQuery, function(twoWeekWarningList, error) {
+            if (error) {
+                return console.log(error);
+            }
+            twoWeekWarningList.forEach(function(twoWeekWarning) {
+                console.log(twoWeekWarning.content);
+            });
+        });
+        if (upgiSystem.list[1].jobList[1].broadcast === true) {
+
+        }
+    }
 }, null, true, config.workingTimezone);
 weeklyMonitorTask.start();
+*/
