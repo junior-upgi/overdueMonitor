fc9fbc3099d7d2494f1ec6e3085c71b701ba8ef5
Chia-Yu Tsai <Chia-Yu Tsai>
2017-02-14 09:10:27 +0800

    fix query because database change

--- src/backend/sql_script/activeSalesStaffDetail.sql
+++ src/backend/sql_script/activeSalesStaffDetail.sql
@@ -1,5 +1,5 @@
 SELECT
-	b.erpID
+	b.SAL_NO AS erpID
 	,c.NAME AS full_name
 	,b.first_name
 	,b.last_name
@@ -7,5 +7,5 @@ SELECT
 	,b.id AS telegramID
 	,b.username AS telegramUsername
 FROM overdueMonitor.dbo.activeSalesStaff a
-	LEFT JOIN telegram.dbo.[user] b ON a.SAL_NO=b.erpID
+	LEFT JOIN telegram.dbo.[user] b ON a.SAL_NO=b.SAL_NO
 	LEFT JOIN DB_U105.dbo.SALM c ON a.SAL_NO=c.SAL_NO;
